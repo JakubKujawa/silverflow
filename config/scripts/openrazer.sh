a52632cf4af18f783b87dc392029aa92e29d588e
Jakub Kujawa <ZK_Jakub@proton.me>
2023-09-28 13:20:50 +0200

    🦄 refactor(openrazer): remove duplicate lines of code
    The `files` module already copies files from files/usr to /usr

--- config/scripts/openrazer.sh
+++ config/scripts/openrazer.sh
@@ -2,7 +2,4 @@
 
 set -oue pipefail
 
-cp /tmp/config/files/usr/lib/udev/rules.d/99-razer.rules /usr/lib/udev/rules.d/99-razer.rules
-cp /tmp/config/files/usr/lib/udev/razer_mount /usr/lib/udev/razer_mount
-
 getent group plugdev >/dev/null || groupadd -r plugdev
