9205403fffb5c5d6bf3f637c84609e73016df19c
Jakub Kujawa <ZK_Jakub@proton.me>
2024-01-27 13:04:29 +0100

    revert: add workaround for gnome-shell to use iGPU instead of dGPU

--- config/scripts/container-tools.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-FILES_ROOT="/tmp/config/files"
-
-wget -O $FILES_ROOT/docker-compose 'https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64'
-install -c -m 0755 "$FILES_ROOT/docker-compose" "/usr/bin/docker-compose"
