b2914523bccf51d9e31191dfbf2a1ca5661f1225
Jakub Kujawa <ZK_Jakub@proton.me>
2024-01-28 19:19:26 +0100

    fix: add missing repository for `tuned-ppd` package

--- config/scripts/power-scheduler.sh
+++ config/scripts/power-scheduler.sh
@@ -5,8 +5,11 @@ set -euo pipefail
 
 wget "https://copr.fedorainfracloud.org/coprs/kylegospo/system76-scheduler/repo/fedora-$(rpm -E %fedora)/kylegospo-system76-scheduler-fedora-$(rpm -E %fedora).repo" \
     -O /etc/yum.repos.d/_copr_kylegospo_system76_scheduler_fedora.repo
-    
-if rpm -qa | grep power-profiles-daemon ; then
+
+wget "https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo?arch=x86_64" \
+    -O /etc/yum.repos.d/_copr_ublue-os-staging.repo
+
+if rpm -qa | grep power-profiles-daemon; then
     rpm-ostree override remove power-profiles-daemon gamemode --install=system76-scheduler --install=gnome-shell-extension-system76-scheduler \
         --install=tuned --install=tuned-ppd --install=tuned-utils --install=tuned-utils-systemtap --install=tuned-profiles-atomic \
         --install=tuned-profiles-cpu-partitioning
@@ -22,3 +25,4 @@ mkdir -p /usr/etc/system76-scheduler
 wget https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/etc/system76-scheduler/config.kdl -O /usr/etc/system76-scheduler/config.kdl
 
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo_system76_scheduler_fedora.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-staging.repo
