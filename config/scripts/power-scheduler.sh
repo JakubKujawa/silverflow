5a67bdb6ec46543a712019e122b59b1fd6dfb3c0
Jakub Kujawa <ZK_Jakub@proton.me>
2024-01-28 12:12:41 +0100

    feat: add TuneD in place of PPD

--- config/scripts/power-scheduler.sh
+++ config/scripts/power-scheduler.sh
@@ -5,22 +5,20 @@ set -euo pipefail
 
 wget "https://copr.fedorainfracloud.org/coprs/kylegospo/system76-scheduler/repo/fedora-$(rpm -E %fedora)/kylegospo-system76-scheduler-fedora-$(rpm -E %fedora).repo" \
     -O /etc/yum.repos.d/_copr_kylegospo_system76_scheduler_fedora.repo
-
-wget "https://copr.fedorainfracloud.org/coprs/szydell/system76/repo/fedora-$(rpm -E %fedora)/szydell-system76-fedora-$(rpm -E %fedora).repo" \
-    -O /etc/yum.repos.d/_copr_szydell_system76_fedora.repo
-
+    
 if rpm -qa | grep power-profiles-daemon ; then
-    rpm-ostree override remove power-profiles-daemon --install=system76-scheduler --install=system76-power --install=gnome-shell-extension-system76-scheduler
+    rpm-ostree override remove power-profiles-daemon gamemode --install=system76-scheduler --install=gnome-shell-extension-system76-scheduler \
+        --install=tuned --install=tuned-ppd --install=tuned-utils --install=tuned-utils-systemtap --install=tuned-profiles-atomic \
+        --install=tuned-profiles-cpu-partitioning
 else
-    rpm-ostree install system76-scheduler system76-power gnome-shell-extension-system76-scheduler
+    rpm-ostree install system76-scheduler gnome-shell-extension-system76-scheduler tuned tuned-ppd tuned-utils tuned-utils-systemtap \
+        tuned-profiles-atomic tuned-profiles-cpu-partitioning
 fi
 
 systemctl enable com.system76.Scheduler.service
-systemctl enable com.system76.PowerDaemon.service
-systemctl enable system76-power-wake.service
+systemctl enable tuned.service
 
 mkdir -p /usr/etc/system76-scheduler
 wget https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/etc/system76-scheduler/config.kdl -O /usr/etc/system76-scheduler/config.kdl
 
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo_system76_scheduler_fedora.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_szydell_system76_fedora.repo
